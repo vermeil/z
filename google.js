1c2f12e53b77e544220cdce9d1fac1bed731734f
vermeil <31437735+vermeil@users.noreply.github.com>
2018-04-02 19:53:52 +0800

    Update google.js

--- google.js
+++ google.js
@@ -237,12 +237,12 @@ function ul_none(){//隐藏ul
 
 //天气预报
 zhh.city =function (){
-  var ct = remote_ip_info["city"]               //先定位一下城市
+  var ct = remote_ip_info["city"];               //先定位一下城市
   console.log(ct);
   var dScr  = document.createElement('script');
   dScr.src = "http://wthrcdn.etouch.cn/weather_mini?city="+ct+"&callback=dcb"; 
   document.body.appendChild(dScr);
-  // document.body.removeChild(dScr);
+ // document.body.removeChild(dScr);
 }
 
 function dcb(data){
