aa5ceab96d00db223a8b82bc6743c83ba5a01836
vermeil <31437735+vermeil@users.noreply.github.com>
2018-07-22 15:07:07 +0800

    Update google.js

--- google.js
+++ google.js
@@ -152,10 +152,8 @@ function addEvent(element, eType, handle, bol) {
     if(element.addEventListener){           //如果支持addEventListener
         bol = bol || 'false';
         element.addEventListener(eType, handle, bol);
-    }else if(element.attachEvent){          //如果支持attachEvent
+    }else {          //如果支持attachEvent
         element.attachEvent("on"+eType, handle);
-    }else{                                  //否则使用兼容的onclick绑定
-        element["on"+eType] = handle;
     }
 }
 // 事件解绑
@@ -163,10 +161,8 @@ function removeEvent(element, eType, handle, bol) {
     if(element.addEventListener){
         bol = bol || 'false';
         element.removeEventListener(eType, handle, bol);
-    }else if(element.attachEvent){
-        element.detachEvent("on"+eType, handle);
     }else{
-        element["on"+eType] = null;
+        element.detachEvent("on"+eType, handle);
     }
 }
 //搜索辅助
